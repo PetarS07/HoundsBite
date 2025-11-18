e5049e6cf22a68611674b54964f00d2b8cd799d8
milenka121 <milenkata_s@abv.bg>
2025-11-18 13:44:26 +0200

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HoundsBite
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "HoundsBite")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "HoundsBite.Pages")]
