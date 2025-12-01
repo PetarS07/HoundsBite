dde7ce4aa562bb60412b84715a6ddbac89ed37fd
Petar Stoilov <mazniq_24@abv.bg>
2025-12-01 03:06:40 +0200

    Add home page and CRUD operations

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.Maui;
+﻿using HoundsBite.Services;
+using HoundsBite.Views;
+using Microsoft.Maui;
 using Microsoft.Maui.Hosting;
-using HoundsBite.Services;
 
 namespace HoundsBite;
 
@@ -15,8 +16,9 @@ public static class MauiProgram
         var dbPath = Path.Combine(FileSystem.AppDataDirectory, "houndsbite.db");
         builder.Services.AddSingleton(new DatabaseService(dbPath));
 
-        builder.Services.AddTransient<IngredientsPage>();
-        builder.Services.AddTransient<RecipesPage>();
+        builder.Services.AddTransient<Views.IngredientsPage>();
+        builder.Services.AddTransient<Views.RecipesPage>();
+        builder.Services.AddTransient<Views.HomePage>();
 
         return builder.Build();
     }
--- Views/IngredientsPage.xaml.cs
+++ Views/IngredientsPage.xaml.cs
@@ -1,11 +1,12 @@
 using HoundsBite.Services;
 using HoundsBite.Models;
 
-namespace HoundsBite;
+namespace HoundsBite.Views;
 
 public partial class IngredientsPage : ContentPage
 {
     private readonly DatabaseService _db;
+    private Ingredient _editingIngredient = null;
 
     public IngredientsPage(DatabaseService db)
     {
@@ -21,9 +22,37 @@ public partial class IngredientsPage : ContentPage
 
     private async void OnAddClicked(object sender, EventArgs e)
     {
-        await _db.Connection.InsertAsync(new Ingredient { Name = IngredientEntry.Text });
-        IngredientList.ItemsSource = await _db.Connection.Table<Ingredient>().ToListAsync();
+        var name = IngredientEntry.Text?.Trim();
+
+        if (string.IsNullOrEmpty(name))
+            return;
+
+        if (_editingIngredient == null)
+        {
+            // CREATE
+            await _db.Connection.InsertAsync(new Ingredient { Name = name });
+        }
+        else
+        {
+            // UPDATE
+            _editingIngredient.Name = name;
+            await _db.Connection.UpdateAsync(_editingIngredient);
+
+            _editingIngredient = null;
+        }
+
         IngredientEntry.Text = "";
+        await RefreshIngredients();
+    }
+    private void OnEditIngredient(object sender, EventArgs e)
+    {
+        var ingredient = (Ingredient)((Button)sender).CommandParameter;
+        _editingIngredient = ingredient;
+        IngredientEntry.Text = ingredient.Name;
+    }
+    private async Task RefreshIngredients()
+    {
+        IngredientList.ItemsSource = await _db.Connection.Table<Ingredient>().ToListAsync();
     }
 
     private async void OnDeleteClicked(object sender, EventArgs e)
